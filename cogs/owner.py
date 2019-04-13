d349eb4e26a1a10fcb3032477cad828a8b433995
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-04-13 18:37:41 +0300

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -37,7 +37,7 @@ class Owner(commands.Cog):
         return content.strip('` \n')
 
     async def __local_check(self, ctx):
-        return await self.bot.is_owner(ctx.author)
+        return await self.bot.is_owner(ctx.author.id)
 
     def get_syntax_error(self, e):
         if e.text is None:
