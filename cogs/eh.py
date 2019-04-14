02f9f7244e0b62358e3cf8b59c906131a1d3575f
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-04-14 17:52:22 +0300

    Update eh.py

--- cogs/eh.py
+++ cogs/eh.py
@@ -1,14 +1,15 @@
 import traceback
 import sys
 from discord.ext import commands
+from utils.settings import ERROR_EMOJI
 import discord
+
 
 
 class CommandErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         """The event triggered when an error is raised while invoking a command.
         ctx   : Context
@@ -24,38 +25,41 @@ class CommandErrorHandler(commands.Cog):
             return
 
         elif isinstance(error, commands.DisabledCommand):
-            return await ctx.send(f'{ctx.command} has been disabled.')
+            return await ctx.send(f'{ERROR_EMOJI} {ctx.command} has been disabled.')
+
+        elif isinstance(error, commands.BadArgument):
+            return await ctx.send(f'{ERROR_EMOJI} {error}.')
+
+        elif isinstance(error, commands.CheckFailure):
+            return await ctx.send(f'{ERROR_EMOJI} {error}.')
+
+        elif isinstance(error, commands.CommandInvokeError):
+            return await ctx.send(f'{ERROR_EMOJI} {error}.')
+
+        elif isinstance(error, commands.TooManyArguments):
+            return await ctx.send(f'{ERROR_EMOJI} {error}.')
+        
+        elif isinstance(error, commands.UserInputError):
+            return await ctx.send(f'{ERROR_EMOJI} {error}.')
+        
+        elif isinstance(error, commands.CommandOnCooldown):
+            return await ctx.send(f'{ERROR_EMOJI} {error}.')
+        
+        elif isinstance(error, commands.NotOwner):
+            return await ctx.send(f'{ERROR_EMOJI} {error}.')
+        
+        elif isinstance(error, commands.MissingPermissions):
+            return await ctx.send(f'{ERROR_EMOJI} {error}.')
+        
+        elif isinstance(error, commands.BotMissingPermissions):
+            return await ctx.send(f'{ERROR_EMOJI} {error}.')
 
         elif isinstance(error, commands.NoPrivateMessage):
             try:
                 return
             except:
                 pass
-        
-        elif isinstance(error, commands.CheckFailure):
-            return await ctx.send(f'{error}.')
-        
-        elif isinstance(error, commands.BadArgument):
-            return await ctx.send(f'{error}.')
-
-        elif isinstance(error, commands.CommandInvokeError):
-            return await ctx.send(f'{error}.')
-
-        elif isinstance(error, commands.TooManyArguments):
-            return await ctx.send(f'{error}.')
                 
-        elif isinstance(error, commands.CommandOnCooldown):
-            return await ctx.send(f'{error}.')
-        
-        elif isinstance(error, commands.NotOwner):
-            return await ctx.send(f'{error}.')
-        
-        elif isinstance(error, commands.MissingPermissions):
-            return await ctx.send(f'{error}.')
-        
-        elif isinstance(error, commands.BotMissingPermissions):
-            return await ctx.send(f'{error}.')
-        
         print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
