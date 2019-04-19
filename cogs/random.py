a97440edd7e5a92e21f6b8859dc1c2857cb5c848
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-04-19 14:26:18 +0300

    Update random.py

--- cogs/random.py
+++ cogs/random.py
@@ -72,7 +72,7 @@ class Random(commands.Cog):
         try:
             async with ctx.typing():
                 b = await self.bot.loop.run_in_executor(None, self.do_dankmeme)
-                await ctx.send(file=discord.Asset(b))
+                await ctx.send(File=discord.Asset(b))
         except Exception as e:
             await ctx.message.add_reaction(ERROR_EMOJI)
             await ctx.send(f'{e}')
