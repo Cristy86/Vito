964fdf5e49e787031dbb7d6ccdebfc178f98fdc1
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-04-19 14:03:23 +0300

    Update random.py

--- cogs/random.py
+++ cogs/random.py
@@ -5,13 +5,24 @@ import asyncio
 import os
 import random
 import aiohttp
+import praw
 from datetime import datetime
-from utils.settings import GREEN_EMBED
+from utils.settings import GREEN_EMBED, ERROR_EMOJI
 from discord.ext.commands.cooldowns import BucketType
 
 class Random(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.reddit = praw.Reddit(client_id=os.getenv('REDDIT_CLIENT_ID'),
+                        client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+                        user_agent=os.getenv('REDDIT_USER_AGENT'))
+
+    def do_dankmeme(self):
+        memes_submissions = self.reddit.subreddit('dankmemes').hot()
+        post_to_pick = random.randint(1, 100)
+        for i in range(0, post_to_pick):
+            submission = next(x for x in memes_submissions if not x.stickied)
+        return submission.url
 
     @commands.command()
     @commands.cooldown(1,5,BucketType.user)
@@ -48,6 +59,23 @@ class Random(commands.Cog):
                     await ctx.send(f"**`{resp['joke']}`**")
         except Exception as e:
             await ctx.send(f"{e}")
+    
+
+    @commands.command()
+    @commands.cooldown(1,5,BucketType.user)
+    @commands.guild_only()
+    async def dankmeme(self, ctx):
+        """Shows a dank meme from r/dankmemes."""
+        if ctx.author.bot:
+            return
+        
+        try:
+            async with ctx.typing():
+                b = await self.bot.loop.run_in_executor(None, self.do_meme)
+                await ctx.send(file=discord.File(b))
+        except Exception as e:
+            await ctx.message.add_reaction(ERROR_EMOJI)
+            await ctx.send(f'{e}')
 
 
 def setup(bot):
