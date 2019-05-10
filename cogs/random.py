49f17c88f684d8e858ff1c5affcb592fc807fb9d
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-05-10 17:42:36 +0300

    Update random.py

--- cogs/random.py
+++ cogs/random.py
@@ -35,7 +35,7 @@ class Random(commands.Cog):
         user = random.choice(ctx.guild.members)
         embed = discord.Embed(color=GREEN_EMBED)
         embed.title = "Random Member"
-        embed.description = f"\U0001f464 User: {self.bot.get_user(user.id)}\n\n\U0001f194 User ID: {user.id}\n\n\U0001f916 Bot: {user.bot}\n\n\U00002139 Joined At: {user.joined_at}"
+        embed.description = f"User: {self.bot.get_user(user.id)}\n\nUser ID: {user.id}\n\nBot: {user.bot}\n\nJoined At: {user.joined_at}"
         embed.set_footer(text=f"{self.bot.user.name}")
         embed.set_thumbnail(url=user.avatar_url)
         embed.timestamp = datetime.utcnow()
