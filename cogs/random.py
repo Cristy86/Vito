8bc4c6883436abcd13d399bd2257c5485bec7136
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2020-02-23 19:11:08 +0200

    Update random.py

--- cogs/random.py
+++ cogs/random.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from utils.paginator import CannotPaginate, SimplePaginator
+from utils.paginator import SimplePaginator
 
 import asyncio
 import os
