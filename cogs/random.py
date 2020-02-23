9237765b875510eda726455f418238626168dfdf
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2020-02-23 19:05:53 +0200

    Update random.py

--- cogs/random.py
+++ cogs/random.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from utils.SimplePaginator import SimplePaginator
+from utils.paginator import HelpPaginator, CannotPaginate
 
 import asyncio
 import os
@@ -108,6 +110,77 @@ class Random(commands.Cog):
         except Exception as e:
             await ctx.message.add_reaction(ERROR_EMOJI)
             await ctx.send(f'```py\n{type(e).__name__}: {str(e)}\n```')
+                                   
+    @commands.command(pass_context=True)
+    @commands.guild_only()
+    @commands.cooldown(1.0, 20.0, commands.BucketType.user)
+    async def userinfo(self, ctx, user: discord.Member = None):
+        """Shows information about a user."""
+
+        try:
+            user = user or ctx.author
+            game = user.activity or None
+
+            if game is None:
+                game = game
+                large_image_game = "https://cdn.discordapp.com/attachments/447662555993866243/464021387481317376/unknown.png"
+                small_image_game = "https://cdn.discordapp.com/attachments/447662555993866243/464021387481317376/unknown.png"
+                large_image_text_game = "None"
+                small_image_text_game = "None"
+            else:
+                game = game
+                large_image_game = game.large_image_url
+                small_image_game = game.small_image_url
+                large_image_text_game = game.large_image_text
+                small_image_text_game = game.small_image_text
+
+            perms = '\n'.join(perm for perm, value in user.guild_permissions if value)
+            days = datetime.utcnow() - user.created_at
+
+            days2 = datetime.utcnow() - user.joined_at
+
+            embed = discord.Embed(color=user.color.value)
+            embed.title = f"`- - {user} - -`"
+            embed.description = f":white_small_square: **Joined at:** **{user.joined_at.strftime('%a %b %d %Y at %I:%M %p')} [`{days2.days} Days.`]**\n:white_small_square: **Status:** **`{user.status}`**\n:white_small_square: **Top Role:** **`{user.top_role.name}`**\n:white_small_square: **Roles:** {','.join([role.name for role in user.roles])}\n:white_small_square: **Playing:** **`{game}`**\n:white_small_square: **Is bot:** **`{user.bot if user.bot else 'False'}`**\n:white_small_square: **ID:** **`{user.id}`**\n:white_small_square: **Created at:** **{user.created_at.strftime('%a %b %d %Y at %I:%M %p')} [`{days.days} Days.`]**"
+            embed.set_thumbnail(url=user.avatar_url)
+            embed.set_footer(text=f"{self.bot.user.name} - Page 1/3")
+            embed.timestamp = datetime.utcnow()
+
+            embed2 = discord.Embed(color=user.color.value)
+            embed2.title = f"`- - {user}'s permissions - -`"
+            embed2.description = f"`{perms}`"
+            embed2.set_footer(text=f"{self.bot.user.name} - Page 2/3")
+
+            embed3 = discord.Embed(color=user.color.value)
+            embed3.title = f"`- - {user}'s game image - -`"
+            embed3.description = f"`Using {small_image_text_game}`\n`{large_image_text_game}`"
+            embed3.set_image(url=large_image_game)
+            embed3.set_footer(text=f"{self.bot.user.name} - Page 3/3")
+            embed3.set_thumbnail(url=small_image_game)
+
+            await SimplePaginator(extras=[embed, embed2, embed3]).paginate(ctx)
+        except:
+            user = user or ctx.author
+            game = user.activity
+
+            perms = '\n'.join(perm for perm, value in user.guild_permissions if value)
+            days = datetime.utcnow() - user.created_at
+
+            days2 = datetime.utcnow() - user.joined_at
+
+            embed = discord.Embed(color=user.color.value)
+            embed.title = f"`- - {user} - -`"
+            embed.description = f":white_small_square: **Joined at:** **{user.joined_at.strftime('%a %b %d %Y at %I:%M %p')} [`{days2.days} Days.`]**\n:white_small_square: **Status:** **`{user.status}`**\n:white_small_square: **Top Role:** **`{user.top_role.name}`**\n:white_small_square: **Roles:** {','.join([role.name for role in user.roles])}\n:white_small_square: **Playing:** **`{game}`**\n:white_small_square: **Is bot:** **`{user.bot if user.bot else 'False'}`**\n:white_small_square: **ID:** **`{user.id}`**\n:white_small_square: **Created at:** **{user.created_at.strftime('%a %b %d %Y at %I:%M %p')} [`{days.days} Days.`]**"
+            embed.set_thumbnail(url=user.avatar_url)
+            embed.set_footer(text=f"{self.bot.user.name} - Page 1/2")
+            embed.timestamp = datetime.utcnow()
+
+            embed2 = discord.Embed(color=user.color.value)
+            embed2.title = f"`- - {user}'s permissions - -`"
+            embed2.description = f"`{perms}`"
+            embed2.set_footer(text=f"{self.bot.user.name} - Page 2/2")
+
+            await SimplePaginator(extras=[embed, embed2]).paginate(ctx)
                                
 def setup(bot):
     bot.add_cog(Random(bot))
