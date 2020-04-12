5443e74fe07234c4b0a6daeb8a67758c4d708f90
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2020-04-12 20:43:13 +0300

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -256,6 +256,11 @@ class Owner(commands.Cog):
         """Nothing here, lol."""
         test = self.cleanup_code(text)                       
         await ctx.send(test)
+    
+    @commands.command(rest_is_raw=True, aliases=['say'])
+    @commands.guild_only()
+    async def echo(self, ctx, *, content):
+        await ctx.send(content)
 
 def setup(bot):
     bot.add_cog(Owner(bot))
