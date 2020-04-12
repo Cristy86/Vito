1f0705accae70a9bafd5e6426783bb8eea9747d7
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2020-04-12 20:28:29 +0300

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -249,6 +249,13 @@ class Owner(commands.Cog):
         """Sets streaming status in silent."""
         await self.bot.change_presence(activity=discord.Streaming(name=activity, url=url))
         await ctx.message.add_reaction(SUCCESS_EMOJI)
+        
+    @commands.command()
+    @commands.guild_only()
+    async def vitochangelog(self, ctx, *, text):
+        """Nothing here, lol."""
+       channel = self.bot.get_channel(582855635817857024)                           
+       await channel.send(f"||<@&698554708247314472>||\n\n{text}")
 
 def setup(bot):
     bot.add_cog(Owner(bot))
