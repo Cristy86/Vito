9a4ec1069061a750b87575b8bbe74e75502aac9d
c_ristian <37700326+Cristy86@users.noreply.github.com>
2020-04-23 15:37:52 +0300

    Update random.py

--- cogs/random.py
+++ cogs/random.py
@@ -217,8 +217,6 @@ class Random(commands.Cog):
     @commands.cooldown(1.0, 3600, commands.BucketType.user)
     async def webhook(self, ctx, user:discord.Member=None, *, text: str):
         """Makes an webhook and sends text as an user."""
-        if user is None:
-            user = ctx.author
         server = self.bot.get_guild(665668439871979520).members
         if ctx.author in server:
              
@@ -240,8 +238,6 @@ class Random(commands.Cog):
     @commands.cooldown(1.0, 30.0, commands.BucketType.user)
     async def dog(self, ctx):
         """Generates a random image dog."""
-        if ctx.author.bot:
-            return
         try:
             async with aiohttp.ClientSession() as cs:
                 async with cs.get('https://random.dog/woof.json') as r:
@@ -261,8 +257,6 @@ class Random(commands.Cog):
     @commands.cooldown(1.0, 30.0, commands.BucketType.user)
     async def cat(self, ctx):
         """Generates a random image cat."""
-        if ctx.author.bot:
-            return
         try:
             async with aiohttp.ClientSession() as cs:
                 async with cs.get('http://aws.random.cat/meow') as r:
@@ -283,8 +277,6 @@ class Random(commands.Cog):
     @commands.cooldown(1.0, 30.0, commands.BucketType.user)
     async def fox(self, ctx):
         """Generates a random image fox."""
-        if ctx.author.bot:
-            return
         try:
             async with aiohttp.ClientSession() as cs:
                 async with cs.get('https://randomfox.ca/floof/') as r:
@@ -304,8 +296,6 @@ class Random(commands.Cog):
     @commands.cooldown(1.0, 30.0, commands.BucketType.user)
     async def hownonce(self, ctx, user: discord.Member = None):
         """How much of an nonce he is?"""
-        if ctx.author.bot:
-            return
         if user is None:
             user = ctx.author
             random.seed(user.id)
@@ -332,9 +322,6 @@ class Random(commands.Cog):
     @commands.cooldown(1.0, 30.0, commands.BucketType.user)
     async def _ball(self, ctx, *, question:str):
         """8ball, what did you expect?"""
-        if ctx.author.bot:
-            return
-        
         results = ["It is certain"," It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes"," Most likely","Outlook good","Yes","Signs point to yes"," Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
         await ctx.send(f"The 🎱 says:\n{random.choice(results)}.")
 
@@ -342,10 +329,7 @@ class Random(commands.Cog):
     @commands.guild_only()
     @commands.check(utils.checks.is_bot)
     @commands.cooldown(1.0, 30.0, commands.BucketType.user)
-    async def rps(self, ctx, *, comment: str):
-        """Rock, paper, scissors!"""
-        if ctx.author.bot:
-            return                                   
+    async def rps(self, ctx, *, comment: str):                                 
         choices = ["Paper", "Scissors", "Rock"]
         try:
             if comment not in choices:
