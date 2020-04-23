22607ac461ac0694845a45ddb55ad268413b23c1
c_ristian <37700326+Cristy86@users.noreply.github.com>
2020-04-23 17:11:26 +0300

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 
 import asyncio
 import os
+from utils.settings import BLUSERS
 
 def is_bot(ctx):
  return ctx.author.bot == False
+
+def is_blacklisted(ctx):
+ return ctx.author.id =! BLUSERS 
